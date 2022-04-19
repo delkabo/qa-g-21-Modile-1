9aadb0ef4d81c9812bb736915b177268d7739afe
delkabo <delkabo@yandex.ru>
2022-04-20 00:26:48 +0300

    added helpers, drivers, test package

---
+++ src/test/java/helpers/BrowserStackMobileDriver.java
@@ -0,0 +1,27 @@
+package helpers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserStackMobileDriver implements WebDriverProvider {
+
+    public static URL getBrowserstackUrl() {
+        try {
+            return new URL("http://hub.browserstack.com/wd/hub");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public WebDriver createDriver(DesiredCapabilities desiredCapabilities) {
+
+        return new AndroidDriver(getBrowserstackUrl(), desiredCapabilities);
+
+    }
+}
