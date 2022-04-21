91aecca330a46d4772ca9e873cc7e768576214dc
delkabo <delkabo@yandex.ru>
2022-04-21 16:23:12 +0300

    add tag

--- src/test/java/tests/BrowserStackAndroidWikiLastTests.java
+++ src/test/java/tests/BrowserStackAndroidWikiLastTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.Properties;
@@ -11,6 +12,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 
+@Tag("selenide")
 public class BrowserStackAndroidWikiLastTests extends TestBase {
 
     @Test
